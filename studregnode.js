91b35aef390d0777d2bf15b6f7fd779f3b36b90e
sowjanya-it <159770816+sowjanya-it@users.noreply.github.com>
2025-04-02 12:39:57 +0530

    Update studregnode.js

--- studregnode.js
+++ studregnode.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
